400202599f25d924e799565df3e88a84d9bfce68
yuetsh <517252939@qq.com>
2022-09-05 15:21:43 +0800

    add author.

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,9 +1,11 @@
 import MDXComponents from "@theme-original/MDXComponents"
 import Editor from "@site/src/components/Editor"
 import BVideo from "@site/src/components/BVideo"
+import Author from "@theme/BlogPostItem/Header/Author"
 
 export default {
   ...MDXComponents,
   Editor,
   BVideo,
+  Author,
 }
